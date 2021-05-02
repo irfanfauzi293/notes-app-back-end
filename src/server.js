a8ae95ab8fc6ceff619e5c7088d05cdd3d18892e
irfanfauzi293 <irfanfauzi293@gmail.com>
2021-05-02 19:56:18 +0700

    wrong ip private amazon value

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ const routes = require('./routes');
 const init = async () => {
   const server = Hapi.server({
     port: 5000,
-    host: process.env.NODE_ENV !== 'production' ? 'localhost' : '172.31.36.19',
+    host: process.env.NODE_ENV !== 'production' ? 'localhost' : '172.31.24.14',
     routes: {
       cors: {
         origin: ['http://ec2-13-212-153-62.ap-southeast-1.compute.amazonaws.com:8000'],
