2b0c99d7b23f4ac9c34221e4181ac837695e5b0b
irfanfauzi293 <irfanfauzi293@gmail.com>
2021-06-12 08:10:49 +0700

    change production server to 0.0.0.0

--- src/server.js
+++ src/server.js
@@ -8,7 +8,7 @@ const init = async () => {
 
   const server = Hapi.server({
     port: 5000,
-    host: process.env.NODE_ENV !== 'production' ? 'localhost' : '172.31.24.14',
+    host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
     routes: {
       cors: {
         origin: ['http://ec2-13-212-153-62.ap-southeast-1.compute.amazonaws.com:8000'],
